dad1c986b007e51a9322909e0aa718e272d35285
hgene2452 <hgene0929@gmail.com>
2024-07-18 20:56:12 +0900

    ✨ event handling

--- section05/src/components/Button.jsx
+++ section05/src/components/Button.jsx
@@ -9,8 +9,14 @@
 
 // 구조 분해 할당을 통해 편리하게 props 사용
 const Button = ({ text, color, children }) => {
+  // 이벤트 객체
+  const onClickButton = (e) => {
+    console.log(e);
+    console.log(text);
+  };
+
   return (
-    <button>
+    <button onClick={onClickButton}>
       {text} - {color.toUpperCase()}
       {children}
     </button>
