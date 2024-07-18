dd33b1d1e6452023d732582c7a1257c7c5681ae4
hgene2452 <hgene0929@gmail.com>
2024-07-18 21:27:56 +0900

    ✨ state & props

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,60 +1,13 @@
 import "./App.css";
-// import Header from "./components/Header";
-// import Main from "./components/Main";
-// import Footer from "./components/Footer";
-// import Button from "./components/Button";
-import { useState } from "react";
+import Bulb from "./components/Bulb";
+import Counter from "./components/Counter";
 
-// function App() {
-//   return (
-//     <>
-//       <Button text={"메일"} color={"red"} />
-//       <Button text={"카페"} />
-//       <Button text={"블로그"} />
-//     </>
-//   );
-// }
-
-// spread 연산자를 이용해 편리하게 props 전달
-// const buttonProps = {
-//   text: "메일",
-//   color: "red",
-//   a: 1,
-//   b: 2,
-//   c: 3,
-// };
-
+// 불필요한 리렌더를 막기 위해 서로 상관없는 컴포넌트는 철저히 분리해야한다
 function App() {
-  const [count, setCount] = useState(0);
-  const [light, setLight] = useState("OFF");
-
   return (
     <>
-      {/* <Button {...buttonProps} />
-      <Button text={"카페"} />
-      <Button text={"블로그"}>
-        <div>자식요소</div>
-      </Button> */}
-      <h1>{count}</h1>
-      <div>
-        <button
-          onClick={() => {
-            setCount(count + 1);
-          }}
-        >
-          +
-        </button>
-      </div>
-      <div>
-        <h1>{light}</h1>
-        <button
-          onClick={() => {
-            setLight(light === "ON" ? "OFF" : "ON");
-          }}
-        >
-          {light === "ON" ? "끄기" : "켜기"}
-        </button>
-      </div>
+      <Bulb />
+      <Counter />
     </>
   );
 }
