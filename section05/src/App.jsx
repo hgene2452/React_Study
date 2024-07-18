232e0dd03e920bf27e4707ea63f222846c99c885
hgene2452 <hgene0929@gmail.com>
2024-07-18 21:10:03 +0900

    ✨ state managing

--- section05/src/App.jsx
+++ section05/src/App.jsx
@@ -1,8 +1,9 @@
 import "./App.css";
-import Header from "./components/Header";
-import Main from "./components/Main";
-import Footer from "./components/Footer";
-import Button from "./components/Button";
+// import Header from "./components/Header";
+// import Main from "./components/Main";
+// import Footer from "./components/Footer";
+// import Button from "./components/Button";
+import { useState } from "react";
 
 // function App() {
 //   return (
@@ -15,22 +16,45 @@ import Button from "./components/Button";
 // }
 
 // spread 연산자를 이용해 편리하게 props 전달
-const buttonProps = {
-  text: "메일",
-  color: "red",
-  a: 1,
-  b: 2,
-  c: 3,
-};
+// const buttonProps = {
+//   text: "메일",
+//   color: "red",
+//   a: 1,
+//   b: 2,
+//   c: 3,
+// };
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [light, setLight] = useState("OFF");
+
   return (
     <>
-      <Button {...buttonProps} />
+      {/* <Button {...buttonProps} />
       <Button text={"카페"} />
       <Button text={"블로그"}>
         <div>자식요소</div>
-      </Button>
+      </Button> */}
+      <h1>{count}</h1>
+      <div>
+        <button
+          onClick={() => {
+            setCount(count + 1);
+          }}
+        >
+          +
+        </button>
+      </div>
+      <div>
+        <h1>{light}</h1>
+        <button
+          onClick={() => {
+            setLight(light === "ON" ? "OFF" : "ON");
+          }}
+        >
+          {light === "ON" ? "끄기" : "켜기"}
+        </button>
+      </div>
     </>
   );
 }
